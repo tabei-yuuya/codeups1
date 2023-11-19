e431a36c24f9d63b0779154758ea6dc36d050563
yuyatabei <ajt.ajt.t@icloud.com>
2023-11-19 13:58:40 +0900

    11/19

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,5 @@
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
-  
+
   var topBtn = $('.page-top');
   $(".page-top").hide();
   // ボタンの表示設定
@@ -120,7 +120,7 @@ $(document).ready(function() {
 
     navigation: {
       nextEl: ".campaign__next",
-      prevEl: ".campaign__prev",
+      prevEl: ".campaign__prev"
     },
   });
 
@@ -128,27 +128,27 @@ $(document).ready(function() {
 
 
   //要素の取得とスピードの設定
-var box = $('.js-colorbox'),
-speed = 700;
+// var box = $('.js-colorbox'),
+// speed = 700;
 
-//.colorboxの付いた全ての要素に対して下記の処理を行う
-box.each(function(){
-$(this).append('<div class="color"></div>')
-var color = $(this).find($('.color')),
-image = $(this).find('img');
-var counter = 0;
+// //.colorboxの付いた全ての要素に対して下記の処理を行う
+// box.each(function(){
+// $(this).append('<div class="color"></div>')
+// var color = $(this).find($('.color')),
+// image = $(this).find('img');
+// var counter = 0;
 
-image.css('opacity','0');
-color.css('width','0%');
-//inviewを使って背景色が画面に現れたら処理をする
-color.on('inview', function(){
-    if(counter == 0){
-     $(this).delay(200).animate({'width':'100%'},speed,function(){
-               image.css('opacity','1');
-               $(this).css({'left':'0' , 'right':'auto'});
-               $(this).animate({'width':'0%'},speed);
-            })
-        counter = 1;
-      }
- });
-});
+// image.css('opacity','0');
+// color.css('width','0%');
+// //inviewを使って背景色が画面に現れたら処理をする
+// color.on('inview', function(){
+//     if(counter == 0){
+//      $(this).delay(200).animate({'width':'100%'},speed,function(){
+//                image.css('opacity','1');
+//                $(this).css({'left':'0' , 'right':'auto'});
+//                $(this).animate({'width':'0%'},speed);
+//             })
+//         counter = 1;
+//       }
+//  });
+// });
