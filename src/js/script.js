1d447dca72168391a05ef3be965f36b1f2f10b59
yuyatabei <ajt.ajt.t@icloud.com>
2024-02-01 15:49:02 +0900

    2/1

--- src/js/script.js
+++ src/js/script.js
@@ -148,30 +148,40 @@ $(document).ready(function() {
   });
 
 
+// tab
+//任意のタブにURLからリンクするための設定
+function GethashID(hashIDName) {
+  if (hashIDName) {
+    $(".js-tab-item")
+      .find("a")
+      .each(function () {
+        //タブ内のaタグ全てを取得
+        var idName = $(this).attr("href"); //タブ内のaタグのリンク名（例）#lunchの値を取得
+        if (idName == hashIDName) {
+          //リンク元の指定されたURLのハッシュタグ（例）http://example.com/#lunch←この#の値とタブ内のリンク名（例）#lunchが同じかをチェック
+          var parentElm = $(this).parent(); //タブ内のaタグの親要素（li）を取得
+          $(".js-tab-item").removeClass("active"); //タブ内のliについているactiveクラスを取り除き
+          $(parentElm).addClass("active"); //リンク元の指定されたURLのハッシュタグとタブ内のリンク名が同じであれば、liにactiveクラスを追加
+          //表示させるエリア設定
+          $(".js-content-area").removeClass("is-active"); //もともとついているis-activeクラスを取り除き
+          $(hashIDName).addClass("is-active"); //表示させたいエリアのタブリンク名をクリックしたら、表示エリアにis-activeクラスを追加
+        }
+      });
+  }
+}
+
+//タブをクリックしたら
+$('.js-tab-item a').on('click', function() {
+	var idName = $(this).attr('href'); //タブ内のリンク名を取得
+	GethashID (idName);//設定したタブの読み込みと
+	return false;//aタグを無効にする
+});
 
 
-  //要素の取得とスピードの設定
-// var box = $('.js-colorbox'),
-// speed = 700;
-
-// //.colorboxの付いた全ての要素に対して下記の処理を行う
-// box.each(function(){
-// $(this).append('<div class="color"></div>')
-// var color = $(this).find($('.color')),
-// image = $(this).find('img');
-// var counter = 0;
-
-// image.css('opacity','0');
-// color.css('width','0%');
-// //inviewを使って背景色が画面に現れたら処理をする
-// color.on('inview', function(){
-//     if(counter == 0){
-//      $(this).delay(200).animate({'width':'100%'},speed,function(){
-//                image.css('opacity','1');
-//                $(this).css({'left':'0' , 'right':'auto'});
-//                $(this).animate({'width':'0%'},speed);
-//             })
-//         counter = 1;
-//       }
-//  });
-// });
+// 上記の動きをページが読み込まれたらすぐに動かす
+$(window).on('load', function () {
+    $('.js-tab-item:first-of-type').addClass("active"); //最初のliにactiveクラスを追加
+    $('.js-content-area:first-of-type').addClass("is-active"); //最初の.areaにis-activeクラスを追加
+	var hashName = location.hash; //リンク元の指定されたURLのハッシュタグを取得
+	GethashID (hashName);//設定したタブの読み込み
+});
